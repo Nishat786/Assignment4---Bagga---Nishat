bed7c1f9962b4df3b384329db4ffb70bcc339e89
unknown <bagganishat111@gmail.com>
2018-08-12 13:48:46 -0400

    Added Bootstrap Support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Home' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { 
+    title: 'Express About',
+    author: 'Nishat Bagga'
+});
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { 
+    title: 'Express Contact Me',
+    author: 'NishatBagga ContactPage'
+});
 });
 
 module.exports = router;
